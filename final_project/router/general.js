2576c7de44eea54d933cd8bda3996d4217291fbf
quanlh87usa <130505800+quanlh87usa@users.noreply.github.com>
2026-05-01 14:02:19 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,7 +52,7 @@ public_users.get("/", function (req, res) {
   }
  });*/
 
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
 
     const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
